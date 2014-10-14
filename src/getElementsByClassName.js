5f4ec1278a21921d047e6881bca59b45fedbfa47
Grant Wu <grantwu2@gmail.com>
2014-10-13 23:28:29 -0700

    getElementsByClassName implemented

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,6 +4,39 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className){
-  // your code here
+
+//Polyfill for string.contains prototype, found in Harmony ECMAScript 6 proposal.
+//See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/contains
+//Could have defined a standalone function but this is cleaner.
+if (!String.prototype.contains) {
+    String.prototype.contains = function() {
+        return String.prototype.indexOf.apply(this, arguments) !== -1;
+    };
+}
+
+var getElementsByClassName = function(targetClass, element){
+    var element = element || document;
+    var result = []; //Returns empty array (which concats into nothing) if targetClass isn't found
+   
+    //If the target class applies to the current element, push the element
+    //to the results array
+    if (element.className && element.className.contains(targetClass)) {
+        result.push(element);
+    } 
+
+    //If the current element has children, make recursive calls to all of its
+    //children and concatenate the results with the main results array.
+    if (element.hasChildNodes()) {
+        var children = element.childNodes;
+        for (var i = 0; i < children.length; i++) {
+            result = result.concat(getElementsByClassName(targetClass, children[i]));
+        }
+    } 
+    return result;
 };
+
+//Personal Notes:
+//In the recursive call, we're always concatenating either an array with a single element inside when the target class is found or an empty array when it isn't. We concatenate ALL of these to the initial result array; the empty arrays disappear, leaving us with one array containing only the matching elements. 
+//Children of elements that match our target class are ignored, as the parent class is immediately returned.
+
+
