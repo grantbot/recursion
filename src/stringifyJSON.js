811eb966b63065c30c6a155407e38535c7a17c31
Grant Wu <grantwu2@gmail.com>
2014-10-13 18:33:56 -0700

    stringifyJSON: Handling Arrays, primitives

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,6 +2,40 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function(obj, index) {
+    var result = ""; 
+    //Handle Strings
+    if (typeof obj === "string") {
+        return '"' + obj + '"';
+    } 
+    
+    //Handle Arrays
+    var index = index || 0;
+    if (Array.isArray(obj) === true) {
+        if (index >= obj.length) {
+            return "[]";
+        }
+        
+        result = stringifyJSON(obj[index]);
+
+        if (index < obj.length - 1) {
+            result = result + "," + stringifyJSON(obj, index + 1);
+        }
+       
+        if (index === 0) {
+            return "[" + result + "]"
+        };
+
+        return result;
+    } 
+    
+    //Handle Objects
+    if (typeof obj === 'object' && obj !== null) {
+        
+    } 
+
+    else {
+        console.log('return2');
+        return result + obj; 
+    }
 };
